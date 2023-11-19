46531ffb72c47b2e97e4d52a6b5a5b0f881773d0
anshuman7negi <anshumannegi777@gmail.com>
2023-11-19 22:30:44 +0530

    Added database and tables

--- db/migrate/20231119163640_create_categories.rb
+++ db/migrate/20231119163640_create_categories.rb
@@ -3,7 +3,7 @@ class CreateCategories < ActiveRecord::Migration[7.1]
     create_table :categories do |t|
       t.string :name
       t.string :icon
-      t.references :author, references: :users, null: false, foreign_key: { to_table: :users }
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.timestamps
     end
   end
--- db/migrate/20231119164949_create_expense_categories.rb
+++ db/migrate/20231119164949_create_expense_categories.rb
@@ -1,8 +1,8 @@
 class CreateExpenseCategories < ActiveRecord::Migration[7.1]
   def change
     create_table :expense_categories do |t|
-      t.references :category, references: :categories, null: false, foreign_key: { to_table: :categories }
-      t.references :expense, references: :expenses, null: false, foreign_key: { to_table: :expenses }
+      t.references :category, null: false, foreign_key: { to_table: :categories }
+      t.references :expense, null: false, foreign_key: { to_table: :expenses }
       t.timestamps
     end
   end
