41cc3f96c776c9ad019dc55e402295f0c1c2de2f
anshuman7negi <anshumannegi777@gmail.com>
2023-11-19 23:09:43 +0530

    Added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root 'categories#index'
 end
