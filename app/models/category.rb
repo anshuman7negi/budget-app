9d7762e9d32c1c3342ba916abd29afb2d3d92886
anshuman7negi <anshumannegi777@gmail.com>
2023-11-20 11:19:22 +0530

    added validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+
+  has_many :expense_categories, dependent: :destroy
+  has_many :expenses, through: :expense_categories, dependent: :destroy
+
+  validates :name, presence: true, length: { in: 5..15 }, uniqueness: true
+  validates :icon, presence: true
 end
