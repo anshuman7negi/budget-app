b0979d41ef7e460c8d1622ce3bd729533c0a63f6
anshuman7negi <anshumannegi777@gmail.com>
2023-11-21 16:31:47 +0530

    Added  splash screen and css

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+document.addEventListener("turbo:load", function() {
+  document.getElementById("loginFormSubmitBtn").addEventListener("click", function() {
+    document.querySelector("form").submit();
+  });
+});
+
+document.addEventListener("turbo:load", function() {
+  document.getElementById('SignupFormSubmitBtn').addEventListener('click', function() {
+    document.getElementById('signup_form').submit();
+  });
+});
+
